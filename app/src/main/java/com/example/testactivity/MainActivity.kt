ec7b1708ba1b753314a59471b9496e46e2e83b5d
al2407111 <nexx2407-7@yandex.ru>
2024-07-29 19:23:07 +0300

    3

--- app/src/main/java/com/example/testactivity/MainActivity.kt
+++ app/src/main/java/com/example/testactivity/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {  companion object {
     const val SEARCH_EDIT_TEXT = "SEARCH_EDIT_TEXT"
     const val SEARCH_EDIT_TEXT_RETAIN = ""
+    const val SEARCH_EDIT_TEXT_RETAIN3 = ""
 }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
