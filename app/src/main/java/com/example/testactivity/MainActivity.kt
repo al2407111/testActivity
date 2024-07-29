7805cff53174779f9f37abdd71ce99472d389a84
al2407111 <nexx2407-7@yandex.ru>
2024-07-29 19:21:40 +0300

    2

--- app/src/main/java/com/example/testactivity/MainActivity.kt
+++ app/src/main/java/com/example/testactivity/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.testactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {  companion object {
+    const val SEARCH_EDIT_TEXT = "SEARCH_EDIT_TEXT"
+    const val SEARCH_EDIT_TEXT_RETAIN = ""
+}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
