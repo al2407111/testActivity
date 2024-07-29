08295dda3004ef7a24aaeb71c0c224b7579bd3aa
al2407111 <nexx2407-7@yandex.ru>
2024-07-29 19:28:42 +0300

    44

--- app/src/main/java/com/example/testactivity/MainActivity.kt
+++ app/src/main/java/com/example/testactivity/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {  companion object {
     const val SEARCH_EDIT_TEXT = "SEARCH_EDIT_TEXT"
     const val SEARCH_EDIT_TEXT_RETAIN = ""
     const val SEARCH_EDIT_TEXT_RETAIN3 = ""
+    const val SEARCH_EDIT_TEXT_RETAIN44 = ""
 }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
